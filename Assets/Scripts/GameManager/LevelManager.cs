4a1071b799d850a11e76374f5d6b4379a18cd258
datnt129 <datngpd@gmail.com>
2023-12-31 23:56:02 +0700

    add singleton

--- Assets/Scripts/GameManager/LevelManager.cs
+++ Assets/Scripts/GameManager/LevelManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelManager : MonoBehaviour
+public class LevelManager : Singleton<LevelManager>
 {
     [SerializeField] private int lives = 5;
 
